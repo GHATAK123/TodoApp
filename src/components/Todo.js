ddef94edfbb9a1b261a2cb51e8a491ee554053d7
Prakash Anand <ppa58514@gmail.com>
2020-09-01 01:20:58 +0530

    delete Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-function Todo() {
+function Todo({todo,text,todos,setTodos}) {
+  const deleteHandler = () => {
+    setTodos(todos.filter(el => el.id !== todo.id))
+           
+  }
   return (
     <div className="todo">
-      <li className="todo-item">HEYYY</li>
+      <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="complete-btn">
+      <button onClick={deleteHandler} className="complete-btn">
       <i className="fas fa-trash"></i>
       </button>
       
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import  Todo from './Todo'
-function TodoList() {
+function TodoList({todos,setTodos}) {
   return (
     <div className="todo-container">
     <ul className="todo-list">
-      <Todo />
+        {todos.map(todo => (
+          <Todo setTodos={setTodos} todo={todo} todos={todos} key={todo.id} text={todo.text}/>
+        ))}
     </ul>
   </div>
   )
